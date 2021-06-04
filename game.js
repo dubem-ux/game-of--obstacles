541412d81be79cb6b97ca3d62e6973b054cd04b1
dubem-ux <dubemrazzle@gmail.com>
2021-06-04 05:22:55 +0100

    today

--- game.js
+++ game.js
@@ -5,26 +5,20 @@ body.onload =(()=>{
     startGame();
 })
 
-
-
 let startGame = (()=>{
-
 // myScore = new component('30px','consolas','black',280,40,'text');
 myGameArea.start();
-myGamePiece = new component(30,30,'red',10,120);
+myGamePiece = new Component(30,30,'red',10,120);
 });
-function component(width, height,color,x,y){
+function Component(width, height,color,x,y){
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
-    console.log(color);
     ctx = myGameArea.context;
-    console.log(ctx)
     context.fillStyle = color;
     context.fillRect(this.x,this.y,this.width,this.height);
 };
-
 let myGameArea = {
     canvas: document.getElementById('canvas'),
     start: (()=>{
@@ -32,12 +26,9 @@ let myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas,document.body.childNodes[0]);
-        this.frameNo = 0;
-        
+        this.frameNo = 0; 
     })
 //     clear:(()=>{
 //         this.context.clearrect(0,0,this.canvas.width,this.canvas.height)
 //     })
 };
-
-
