87ecb69b672e970f042e602e8137cc2a2ef3ce5c
dubem-ux <dubemrazzle@gmail.com>
2021-06-11 20:29:11 +0100

    started moving

--- game.js
+++ game.js
@@ -13,11 +13,23 @@ myGamePiece = new Component(30,30,'red',10,120);
 function Component(width, height,color,x,y){
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
     this.x = x;
     this.y = y;
-    ctx = myGameArea.context;
-    context.fillStyle = color;
-    context.fillRect(this.x,this.y,this.width,this.height);
+   
+    this.update = (()=>{
+
+        ctx = myGameArea.context;
+        context.fillStyle = color;
+        context.fillRect(this.x,this.y,this.width,this.height);
+    })
+
+    this.pos = (()=>{
+        this.x += this.speedX;
+        this.y += this.speedY;
+    })
+    
 };
 let myGameArea = {
     canvas: document.getElementById('canvas'),
@@ -26,9 +38,15 @@ let myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas,document.body.childNodes[0]);
-        this.frameNo = 0; 
+        this.interval = setInterval(updateGameArea,20)
+    }),
+    clear:(()=>{
+        this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
     })
-//     clear:(()=>{
-//         this.context.clearrect(0,0,this.canvas.width,this.canvas.height)
-//     })
 };
+
+let updateGameArea = (()=>{
+    myGameArea.clear();
+    myGamePiece.x += 1
+    myGamePiece.update()
+});
